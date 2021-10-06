cd1191c335fb8eb7e999c4dd368aa41792a4cf32
Juliana Fernandez <julianafernandezlima@hotmail.com>
2021-10-06 06:09:00 -0300

    Adding whole project

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+const watchContainer = document.querySelector('.watch-container')
+
+
+const reloadClock = () => {
+    const present = new Date()
+    
+    const hour = present.getHours()
+    const minutes = present.getMinutes()
+    const seconds = present.getSeconds()
+
+    const watchHTML = `
+    <span>${String(hour).length === 1 ? `0${hour}`: hour}</span> :
+    <span>${String(minutes).length === 1 ? `0${minutes}`: hour}</span> :
+    <span>${String(seconds).length === 1 ? `0${seconds}`: seconds}</span>
+    `
+
+    watchContainer.innerHTML = watchHTML
+}
+
+setInterval(reloadClock, 1000)
